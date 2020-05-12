0ab4e7c0ac585af7118fae268b871102a67ebf4e
vehikl <vehikl@vehikl.com>
2020-05-12 15:58:07 -0400

    Extract magic numbers

--- php/TennisGame2.php
+++ php/TennisGame2.php
@@ -2,6 +2,11 @@
 
 class TennisGame2 implements TennisGame
 {
+    private const MAX_SCORE = 4;
+    private const LOVE_SCORE = 0;
+    private const THIRTY_SCORE = 2;
+    private const FORTY_SCORE = 3;
+    private const FIFTEEN_SCORE = 1;
     private $P1point = 0;
     private $P2point = 0;
 
@@ -19,31 +24,31 @@ class TennisGame2 implements TennisGame
     public function getScore()
     {
         $score = "";
-        if ($this->P1point == $this->P2point && $this->P1point < 4) {
-            if ($this->P1point == 0) {
+        if ($this->P1point == $this->P2point && $this->P1point < self::MAX_SCORE) {
+            if ($this->P1point == self::LOVE_SCORE) {
                 $score = "Love";
             }
-            if ($this->P1point == 1) {
+            if ($this->P1point == self::FIFTEEN_SCORE) {
                 $score = "Fifteen";
             }
-            if ($this->P1point == 2) {
+            if ($this->P1point == self::THIRTY_SCORE) {
                 $score = "Thirty";
             }
             $score .= "-All";
         }
 
-        if ($this->P1point == $this->P2point && $this->P1point >= 3) {
+        if ($this->P1point == $this->P2point && $this->P1point >= self::FORTY_SCORE) {
             $score = "Deuce";
         }
 
-        if ($this->P1point > 0 && $this->P2point == 0) {
-            if ($this->P1point == 1) {
+        if ($this->P1point > self::LOVE_SCORE && $this->P2point == self::LOVE_SCORE) {
+            if ($this->P1point == self::FIFTEEN_SCORE) {
                 $this->P1res = "Fifteen";
             }
-            if ($this->P1point == 2) {
+            if ($this->P1point == self::THIRTY_SCORE) {
                 $this->P1res = "Thirty";
             }
-            if ($this->P1point == 3) {
+            if ($this->P1point == self::FORTY_SCORE) {
                 $this->P1res = "Forty";
             }
 
@@ -51,65 +56,65 @@ class TennisGame2 implements TennisGame
             $score = "{$this->P1res}-{$this->P2res}";
         }
 
-        if ($this->P2point > 0 && $this->P1point == 0) {
-            if ($this->P2point == 1) {
+        if ($this->P2point > self::LOVE_SCORE && $this->P1point == self::LOVE_SCORE) {
+            if ($this->P2point == self::FIFTEEN_SCORE) {
                 $this->P2res = "Fifteen";
             }
-            if ($this->P2point == 2) {
+            if ($this->P2point == self::THIRTY_SCORE) {
                 $this->P2res = "Thirty";
             }
-            if ($this->P2point == 3) {
+            if ($this->P2point == self::FORTY_SCORE) {
                 $this->P2res = "Forty";
             }
             $this->P1res = "Love";
             $score = "{$this->P1res}-{$this->P2res}";
         }
 
-        if ($this->P1point > $this->P2point && $this->P1point < 4) {
-            if ($this->P1point == 2) {
+        if ($this->P1point > $this->P2point && $this->P1point < self::MAX_SCORE) {
+            if ($this->P1point == self::THIRTY_SCORE) {
                 $this->P1res = "Thirty";
             }
-            if ($this->P1point == 3) {
+            if ($this->P1point == self::FORTY_SCORE) {
                 $this->P1res = "Forty";
             }
-            if ($this->P2point == 1) {
+            if ($this->P2point == self::FIFTEEN_SCORE) {
                 $this->P2res = "Fifteen";
             }
-            if ($this->P2point == 2) {
+            if ($this->P2point == self::THIRTY_SCORE) {
                 $this->P2res = "Thirty";
             }
             $score = "{$this->P1res}-{$this->P2res}";
         }
 
-        if ($this->P2point > $this->P1point && $this->P2point < 4) {
-            if ($this->P2point == 2) {
+        if ($this->P2point > $this->P1point && $this->P2point < self::MAX_SCORE) {
+            if ($this->P2point == self::THIRTY_SCORE) {
                 $this->P2res = "Thirty";
             }
-            if ($this->P2point == 3) {
+            if ($this->P2point == self::FORTY_SCORE) {
                 $this->P2res = "Forty";
             }
-            if ($this->P1point == 1) {
+            if ($this->P1point == self::FIFTEEN_SCORE) {
                 $this->P1res = "Fifteen";
             }
-            if ($this->P1point == 2) {
+            if ($this->P1point == self::THIRTY_SCORE) {
                 $this->P1res = "Thirty";
             }
             $score = "{$this->P1res}-{$this->P2res}";
         }
 
-        if ($this->P1point > $this->P2point && $this->P2point >= 3) {
+        if ($this->P1point > $this->P2point && $this->P2point >= self::FORTY_SCORE) {
             $score = "Advantage player1";
         }
 
-        if ($this->P2point > $this->P1point && $this->P1point >= 3) {
+        if ($this->P2point > $this->P1point && $this->P1point >= self::FORTY_SCORE) {
             $score = "Advantage player2";
         }
 
-        if ($this->P1point >= 4 && $this->P2point >= 0 && ($this->P1point - $this->P2point) >= 2) {
+        if ($this->P1point >= self::MAX_SCORE && $this->P2point >= self::LOVE_SCORE && ($this->P1point - $this->P2point) >= self::THIRTY_SCORE) {
             $score = "Win for player1";
         }
 
-        if ($this->P2point >= 4 && $this->P1point >= 0 && ($this->P2point - $this->P1point) >= 2) {
+        if ($this->P2point >= self::MAX_SCORE && $this->P1point >= self::LOVE_SCORE && ($this->P2point - $this->P1point) >= self::THIRTY_SCORE) {
             $score = "Win for player2";
         }
 
